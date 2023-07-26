43f31b6cacdf00488ee4233379e3945b016f1fca
maclenfraser <89809061+maclenfraser@users.noreply.github.com>
2023-07-26 03:48:26 +0000

    Updates

--- coffee_shop_maclen/models/customers.sql
+++ coffee_shop_maclen/models/customers.sql
@@ -8,7 +8,7 @@ with customer_orders as (
      , count(*) as n_orders
      , min(created_at) as first_order_at
 
-  from `analytics-engineers-club.coffee_shop.orders` 
+  from {{ source('coffee_shop', 'orders') }} as orders
   group by 1
 )
 
@@ -18,6 +18,6 @@ select
   , customers.email
   , coalesce(customer_orders.n_orders, 0) as n_orders
   , customer_orders.first_order_at
-from `analytics-engineers-club.coffee_shop.customers` as customers
+from {{ source('coffee_shop', 'customers') }} as customers
 left join  customer_orders
   on  customers.id = customer_orders.customer_id 
